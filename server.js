b6bd92086d1f1a79a876e8d273f3420ab15d0d37
Caesar Lennartsson <caesar.1999@hotmail.com>
2021-07-08 14:21:13 +0200

    Implement fetch-functionality and render data with ejs from the api. Update styling

--- server.js
+++ server.js
@@ -15,8 +15,32 @@ app.set('views', join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(join(__dirname, '/public')))
 
+const fetchNews = async () => {
+  try {
+    const main = 'top-headlines'
+    const category = 'technology'
+    const language = 'en'
+    const apiKey = process.env.API_KEY
+    const url = `https://newsapi.org/v2/${main}?category=${category}&language=${language}`
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: apiKey,
+      },
+    })
+    return response
+  } catch (error) {
+    return { status: 500, statusText: 'fail', data: [] }
+  }
+}
+
 app.get('/', async (req, res) => {
-  res.render('pages/index.ejs')
+  const result = await fetchNews()
+  const { status, statusText, data } = result
+  const articles = data.articles.slice(0, 10)
+  // Error handling (check response status-code)
+  res.render('pages/index.ejs', {
+    articles,
+  })
 })
 
 app.listen(PORT, () => {
