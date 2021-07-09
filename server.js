48dbdcfae6576fd6c61397d9780aa3fbbfa67103
Caesar Lennartsson <caesar.1999@hotmail.com>
2021-07-09 23:22:20 +0200

    Make fetch-function more general and implement functionality for search-field

--- server.js
+++ server.js
@@ -14,14 +14,13 @@ const app = express()
 app.set('views', join(__dirname, '/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(join(__dirname, '/public')))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-const fetchNews = async () => {
+const fetchNews = async (url) => {
   try {
-    const main = 'top-headlines'
-    const category = 'technology'
-    const language = 'en'
     const apiKey = process.env.API_KEY
-    const url = `https://newsapi.org/v2/${main}?category=${category}&language=${language}`
+
     const response = await axios.get(url, {
       headers: {
         Authorization: apiKey,
@@ -29,16 +28,34 @@ const fetchNews = async () => {
     })
     return response
   } catch (error) {
+    console.log(error)
     return { status: 500, statusText: 'fail', data: [] }
   }
 }
 
 app.get('/', async (req, res) => {
-  const result = await fetchNews()
+  const main = 'top-headlines'
+  const url = `https://newsapi.org/v2/${main}?category=technology&language=en`
+  const result = await fetchNews(url)
   const { status, statusText, data } = result
   const articles = data.articles.slice(0, 10)
   // Error handling (check response status-code)
-  res.render('pages/index.ejs', {
+  return res.render('pages/index.ejs', {
+    articles,
+  })
+})
+
+app.get('/search', async (req, res) => {
+  const query = req.query.keyword
+  if (!query || query.length < 1) return res.redirect('/')
+
+  const main = 'top-headlines'
+  const url = `https://newsapi.org/v2/${main}?category=technology&language=en&q=${query}`
+  const result = await fetchNews(url)
+  const { status, statusText, data } = result
+  const articles = data.articles.slice(0, 10)
+  // Error handling (check response status-code)
+  return res.render('pages/index.ejs', {
     articles,
   })
 })
